f9c554bbdad7583ed929bfd126a08ead74c5677a
Chris Smith <chris@cgsmith.net>
2016-06-03 13:14:35 -0500

    PEP8 updates

--- button.py
+++ button.py
@@ -6,37 +6,38 @@ import requests
 import time
 
 # All of the configuration should be done within here
-apikey =  'your-api-key' #api key
-fromemail = 'from@email.com' #from address
-tomailbox = ['to@email.com'] #email for sending mailbox open alert
-toannounce = ['announce@email.com'] #announcement email of external IP
+apikey = 'your-api-key'  # api key
+fromemail = 'from@email.com'  # from address
+tomailbox = ['to@email.com']  # email for sending mailbox open alert
+toannounce = ['announce@email.com']  # announcement email of external IP
 subjectname = '[Subject in the email]'
 buttonmsg = 'Hello,<br><br>The mailbox button was pressed'
 fqn = os.uname()[1]
 r = requests.get('http://wtfismyip.com/json')
-startup = "Hello,<br><br>The controller (" + fqn + ") powered on just now.  Is this happening often? Here is the external IP for SSH access: " + r.json()['YourFuckingIPAddress'] #THATS THE NAME! I swear
+startup = "Hello,<br><br>The controller (" + fqn + ") powered on just now.  Is this happening often? Here is the external IP for SSH access: " + \
+          r.json()['YourFuckingIPAddress']  # THATS THE NAME! I swear
 
-#---------------------------------------
+# ---------------------------------------
 # No modifications necessary below here
 
 print('Powering controller on...')
 
-trigger = int(time.time()) # Time used to see how long it has been since last button press
+trigger = int(time.time())  # Time used to see how long it has been since last button press
 
 try:
-  sparky = SparkPost(apikey) # Connect up to SparkPost
+    sparky = SparkPost(apikey)  # Connect up to SparkPost
 
-  # send off the external IP for SSH in the future 
-  # Have sparky issue a message that the controller just powered on
-  response = sparky.transmissions.send(
-     recipients=toannounce,
-     html=startup,
-     from_email=fromemail,
-     subject=subjectname + ' Controller Power On')
+    # send off the external IP for SSH in the future
+    # Have sparky issue a message that the controller just powered on
+    response = sparky.transmissions.send(
+        recipients=toannounce,
+        html=startup,
+        from_email=fromemail,
+        subject=subjectname + ' Controller Power On')
 
 except SparkPostException, e:
-  print('A mailing error occurred: %s - %s' %  (e.__class__, e))
-  raise
+    print('A mailing error occurred: %s - %s' % (e.__class__, e))
+    raise
 
 # Setup the GPIO mode and PIN for 16
 GPIO.setmode(GPIO.BOARD)
@@ -47,30 +48,29 @@ print('GPIO set for 16')
 try:
     print 'Trying to enter loop to check for button press'
     while True:
-        time.sleep(0.05) # Maintain a safe distance from button presses
-        if (GPIO.input(16) == 1): # it is pushed - just print it
+        time.sleep(0.05)  # Maintain a safe distance from button presses
+        if GPIO.input(16) == 1:  # it is pushed - just print it
             print('Button pressed time is :')
             print(trigger)
             print(time.time())
-        if ((GPIO.input(16) == 1) & ((trigger + 3) < (int(time.time())))):
+        if (GPIO.input(16) == 1) & ((trigger + 3) < (int(time.time()))):
             trigger = int(time.time())
             try:
-              print('Time was less then trigger')
-              print('Attempting to send email')
-              sparky = SparkPost(apikey) # Connect up to SparkPost
+                print('Time was less then trigger')
+                print('Attempting to send email')
+                sparky = SparkPost(apikey) # Connect up to SparkPost
 
-              # Send off mailbox opened message
-              response = sparky.transmissions.send(
-                 recipients=tomailbox,
-                 html=buttonmsg,
-                 from_email=fromemail,
-                 subject=subjectname)
+                # Send off mailbox opened message
+                response = sparky.transmissions.send(
+                    recipients=tomailbox,
+                    html=buttonmsg,
+                    from_email=fromemail,
+                    subject=subjectname)
 
-              print 'Email was sent'
+                print 'Email was sent'
             except SparkPostException, e:
-              print('A mailing error occurred: %s - %s' %  (e.__class__, e))
-              raise
+                print('A mailing error occurred: %s - %s' % (e.__class__, e))
+                raise
 
 except KeyboardInterrupt:
     GPIO.cleanup()
-
